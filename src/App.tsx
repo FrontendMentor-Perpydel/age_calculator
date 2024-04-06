a9a7ff8d81b8af3949901ec87507882f87d1abea
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-04-06 20:24:44 +0100

    init: initialized project and setup basics

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      Day DD Month MM Year YYYY -- years -- months -- days
+      <div className="attribution">
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
